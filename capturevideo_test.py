1fade195f8ab5ab37204ae27c505a88b8ec5018f
Lutecea <oceane.piatpro@gmail.com>
2025-09-18 15:51:27 +0200

    Video seule
    Fonctionne.

--- capturevideo_test.py
+++ capturevideo_test.py
@@ -18,15 +18,15 @@ Drone = tello.Tello()
 Drone.connect()
 
 #Get Battery Info
-print(Drone.get_battery())
+print(Drone.query_battery())
 
 #Start Camera Display Stream
 Drone.streamon()
 while True:
 
-#Get Frame From Drone Camera Camera 
+# #Get Frame From Drone Camera Camera 
     img = Drone.get_frame_read().frame
     img = cv2.resize(img, (1080,720))
-#Show The Frame
+# #Show The Frame
     cv2.imshow("DroneCapture", img)
     cv2.waitKey(1)
