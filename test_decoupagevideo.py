7678b10f0aeb93a26270e43628eca6e2cb6dd466
Lutecea <oceane.piatpro@gmail.com>
2025-10-07 11:47:08 +0200

    Update test_decoupagevideo.py
    Enregistrement en matrice fonctionnel mais trop volumineux

--- test_decoupagevideo.py
+++ test_decoupagevideo.py
@@ -6,17 +6,25 @@ Created on Tue Oct  7 08:40:41 2025
 """
 
 import cv2
+import numpy 
 
-vidcap = cv2.VideoCapture('output\output.mp4')
+path = "output/"
+f = open(path+"matrices.txt", "a")
+
+vidcap = cv2.VideoCapture(path + 'output.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frame%d.png" % count, image)     # save frame as JPEG file      
-  #Conversion en matrice
-  img = cv2.imread("frame%d.png", 0)
-  #TODO ENREGISTRER LES FICHIERS 
+  cv2.imwrite(path + "frame%d.png" % count, image)
   success,image = vidcap.read()
   count += 1
   print('Frame :  ' ,count , success)
+  #Conversion en matrice
+for i in range(15) :
+    # print("frame" + str(i) + ".png")
+    img = cv2.imread(path+ "frame" + str(i) + ".png", 0)
+    numpy.savetxt(f, img)
+    
   
+f.close()
 print("Terminé")
